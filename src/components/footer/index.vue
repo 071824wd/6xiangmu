7b4a89360d07c84f44a8e799c865b9a1115b31d3
bai <3308054198@qq.com>
2019-10-14 20:32:45 +0800

    今晚添加

--- src/components/footer/index.vue
+++ src/components/footer/index.vue
@@ -1,9 +1,31 @@
 <template>
     <div class="footer">
-        <router-link to="/shou" tag="p">首页</router-link>
-        <router-link to="/faxian"  tag="p">发现</router-link>
-        <router-link to="/cart"  tag="p">购物车</router-link>
-        <router-link to="/wode"  tag="p">我的</router-link>
+        <ul>
+            <router-link to="/shou" tag="li">
+                 <svg class="icon" aria-hidden="true">
+                    <use xlink:href="#icon-zhuye"></use>
+                </svg>
+                <a href="#">首页</a>
+            </router-link>
+            <router-link to="/faxian" tag="li">
+                 <svg class="icon" aria-hidden="true">
+                    <use xlink:href="#icon-zhuye"></use>
+                </svg>
+                <a href="#">发现</a>
+            </router-link>
+            <router-link to="/cart" tag="li">
+                 <svg class="icon" aria-hidden="true">
+                    <use xlink:href="#icon-zhuye"></use>
+                </svg>
+                <a href="#">购物车</a>
+            </router-link>
+            <router-link to="/wode" tag="li">
+                 <svg class="icon" aria-hidden="true">
+                    <use xlink:href="#icon-zhuye"></use>
+                </svg>
+                <a href="#">我的</a>
+            </router-link>
+        </ul>
     </div>
 </template>
 <script>
@@ -14,9 +36,34 @@ export default {
 <style>
     .footer{
         width: 100%;
-        height: 1.14rem;
-        display: flex;
-        justify-content: space-around;
-        align-items: center;
+        height: 60px;
+        border-top: 1px solid rgb(226, 226, 226);
+        position: fixed;
+        bottom: 0px;
+        background: white;
     }
+    .footer ul{
+        display: flex;
+    }
+    .footer ul li{
+        width: 25%;
+        height: 60px;
+    }
+    .footer ul li .icon{
+        width: 25px;
+        height: 25px;
+        fill: currentColor;
+        display: block;
+        padding-top: 5px;
+        margin: 0px auto;
+    }
+    .footer ul li a{
+        display: block;
+        text-decoration: none;
+        color: black; 
+        font-size: 16px;
+        padding-top: 4px;
+        text-align: center;
+    }
+
 </style>
