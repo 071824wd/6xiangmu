df7e2868b3fd869a924f4c8f384cdf80eef9ff0a
张 <zhang@example.com>
2019-10-14 19:16:39 +0800

    提交

---
+++ src/paser/faxian.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="main">
+        <Header></Header>
+        <div class="m-d"></div>
+        <Footer></Footer>
+    </div>
+</template>
+<script>
+import Header from '../components/header'
+import Footer from '../components/footer'
+export default {
+     components: {
+   Header,
+   Footer
+  }
+}
+</script>
+<style>
+    
+</style>
